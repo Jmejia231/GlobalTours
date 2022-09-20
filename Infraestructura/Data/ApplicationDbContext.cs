08aca0dea7b49e007dc6893a7a8e310b9f5329c3
Julio <julmejiar@upt.pe>
2022-09-19 23:08:49 -0500

    Commit Inicial

---
+++ Infraestructura/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructura.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Lugar> Lugar { get; set; }
+    }
+}
